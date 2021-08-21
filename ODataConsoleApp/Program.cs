8489fea046f1182a0a49b0536ff43d46fb809822
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-08-21 08:38:18 +0300

    ilerleme sağlandı

---
+++ ODataConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Default;
+using System;
+
+namespace ODataConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceRoot = "https://localhost:44315/odata";
+            var context = new Container(new Uri(serviceRoot));
+            var products = context.Products.Execute();
+            var filteredProducts = context.Products.AddQueryOption("$filter", "Id eq 1");
+            foreach (var item in filteredProducts)
+            {
+                Console.WriteLine(item.Name);
+            }
+            
+        }
+    }
+}
